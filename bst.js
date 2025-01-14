5ec6abd52df079cfe384f52f624c5dd2053c9c6c
kad5 <khaled.mohamed.1807@gmail.com>
2025-01-14 18:53:34 +0200

    added find method that traveses the tree to search a value

--- bst.js
+++ bst.js
@@ -11,18 +11,25 @@ class Tree {
     const sorted = prepArray(array);
     this.root = buildTree(sorted);
   }
+
+  insert(value) {}
+  deleteItem(value) {}
+
+  find(value, node = this.root) {
+    if (value === node.data) return node;
+    if (value < node.data && node.left) return this.find(value, node.left);
+    if (value > node.data && node.right) return this.find(value, node.right);
+    return "not in the tree";
+  }
 }
 
 function buildTree(array) {
-  if (array.length <= 1) return createNode(array[0]);
-
+  if (array.length === 0) return null;
   const mid = Math.floor(array.length / 2);
-  const left = array.slice(0, mid);
-  const right = array.slice(mid, array.length);
-
   const root = createNode(array[mid]);
-  root.left = buildTree(left);
-  root.right = buildTree(right);
+
+  root.left = buildTree(array.slice(0, mid));
+  root.right = buildTree(array.slice(mid + 1));
 
   return root;
 }
@@ -63,5 +70,18 @@ function merge(left, right) {
   return result.concat(left.slice(i)).concat(right.slice(j));
 }
 
-const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-//console.log(myArray)
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const myRoot = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+prettyPrint(myRoot.root);
